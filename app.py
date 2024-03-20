fc0f20c63ccb9366bb7cc64e5a1c214749e6ca23
AriyoX <ahumuzaariyo@gmail.com>
2024-03-20 19:52:05 +0300

    Made significant changes

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 # Sample data for products
 products = [
-    {"id": 1, "name": "Simple Glam", "price": 30000, "image_path": "images/simple_glam.jpg"},
-    {"id": 2, "name": "Full Face Beat", "price": 50000, "image_path": "images/full_face_beat.jpg"},
-    {"id": 3, "name": "Eye Glam", "price": 10000, "image_path": "images/eye_glam.jpg"}
+    {"id": 1, "name": "Simple Glam", "price": 30000, "image_path": "images/simple_glam.jpg", "description": "A simple glam makeup look involves a flawless base with foundation and concealer, defined brows, neutral eyeshadow shades, winged eyeliner, mascara-coated lashes, a touch of blush, and a nude lipstick or gloss for a polished finish."},
+    {"id": 2, "name": "Full Face Beat", "price": 50000, "image_path": "images/full_face_beat.jpg", "description": "A full face beat entails flawless complexion with foundation, concealer, and powder, sculpted brows, defined eyes with eyeshadow, liner, and mascara, contoured cheeks, highlighted features, and a statement lip color, finished with setting spray for longevity and a flawless finish."},
+    {"id": 3, "name": "Eye Glam", "price": 10000, "image_path": "images/eye_glam.jpg", "description":"Eye glam features intricate eyeshadow blending, dramatic eyeliner (winged or smoked), voluminous mascara, and optional false lashes for added drama. Sparkling or metallic shades enhance the lids, while dark hues deepen the crease. Highlighter on the inner corner brightens, completing the mesmerizing look."}
 ]
 
 # Cart to store selected products
@@ -14,7 +14,8 @@ cart = []
 
 @app.route('/')
 def home():
-    return render_template('home.html', products=products)
+    cart_count = len(cart)
+    return render_template('home.html', products=products, cart_count=cart_count)
 
 @app.route('/product/<int:id>')
 def product(id):
@@ -46,8 +47,11 @@ def view_cart():
 
 @app.route('/checkout', methods=['POST'])
 def checkout():
-    total_price = sum(product['price'] for product in cart)
-    return render_template("checkout.html", cart=cart, total_price=total_price)
+    if len(cart) == 0:  # Check if the cart is empty
+        return redirect(url_for('home'))  # Redirect to home if cart is empty
+    else:
+        total_price = sum(product['price'] for product in cart)
+        return render_template("checkout.html", cart=cart, total_price=total_price)
 
 @app.route('/process_payment', methods=['POST'])
 def process_payment():
